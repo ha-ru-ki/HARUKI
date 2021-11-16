fbcbad2f6b2ed9640e23279de856cbd0b11eea84
ha-ru-ki <nishiguchi-haruki-xg@ynu.jp>
2021-11-16 15:24:47 +0900

    カリキュラム08-4

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest; // useする
 use App\Post;
 
 
@@ -12,16 +12,25 @@ class PostController extends Controller
     public function index(Post $post)
 
    {
-    return view('index')->with(['posts' => $post->getPaginateByLimit(4)]); //view('index')のindexは拡張子 index.blade.phpの
+    return view('index')->with(['posts' => $post->getPaginateByLimit(4)]); 
    } 
    
-  public function show(Post $post)
+    public function show(Post $post)
    {
        return view('show')->with(['post' => $post]);
    }
    
+    public function create()
+    {
+        return view('create');
+    }  
 
-
+    public function store(PostRequest $request, Post $post)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
    
 }
 
